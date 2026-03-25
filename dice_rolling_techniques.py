5a51578568b9dc45d89c83d5d831ea48774b743f
Akhila <102972761+akhila8978@users.noreply.github.com>
2026-03-25 21:31:09 +0530

    Enhance dice rolling to allow multiple rolls
    Added functionality to choose the number of times to roll the dice.

--- dice_rolling_techniques.py
+++ dice_rolling_techniques.py
@@ -12,4 +12,25 @@ while True:
     else:
         print("Invalid choice!")
 
+
+----- to choose no of times to roll the dice -----
+
+import random
+
+while True:
+    choice=input('Roll the dice ? (y/n):').lower()
+    if choice =='y':
+        roll=int(input())
+        for i in range(roll):
+            die1 = random.randint(1,6)
+            die2 = random.randint(1,6)
+            print(f' roll{i+1} : {die1},{die2}')
+    elif choice == 'n':
+        print("THANKS FOR PLAYING")
+        break
+    else:
+        print("Invalid choice!")
+
+            
+
             
